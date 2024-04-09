534818368366b1346dc36fd6e3830ae31c5ec967
emkova <165190698+emkova@users.noreply.github.com>
2024-04-09 11:28:16 -0300

    Revert to original init_db.py

--- init_db.py
+++ init_db.py
@@ -8,7 +8,7 @@ with open('schema.sql') as f:
     script = f.read()
     cur.executescript(script)
 
-cur.execute("INSERT INTO prompts (prompt, msg) VALUES ('This is a test prompt.', 'This is a test message.')")
+cur.execute("INSERT INTO prompts (reflection, msg) VALUES ('This is a test reflection.', 'This is a test message.')")
 
 connection.commit()
 connection.close()
